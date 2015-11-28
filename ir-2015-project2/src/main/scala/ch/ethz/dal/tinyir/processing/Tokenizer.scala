23caba9ad3cc0dc46c92fcd5305639504c0e5a11
sarahdaniel <sarah.daniel.nassif@gmail.com>
2015-11-28 14:31:25 +0100

    Extended the tokenizer to remove "-"

--- ir-2015-project2/src/main/scala/ch/ethz/dal/tinyir/processing/Tokenizer.scala
+++ ir-2015-project2/src/main/scala/ch/ethz/dal/tinyir/processing/Tokenizer.scala
@@ -2,5 +2,5 @@ package ch.ethz.dal.tinyir.processing
 
 object Tokenizer {
   def tokenize (text: String) : List[String] =
-    text.split("[ .,;:?!/\t\n\r\f]+").toList.map(t=> t.toLowerCase())
+    text.split("[ -.,;:?!/\t\n\r\f]+").toList.map(t=> t.toLowerCase())
 }
